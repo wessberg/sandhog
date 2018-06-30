c4b491572d422aadd892cfb8235233a254a5c640
wessberg <frederikwessberg@gmail.com>
2018-06-30 15:20:23 +0200

    Updated configuration

--- src/util/non-bool-env-var-exists/non-bool-env-var-exists.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Returns true if the given (non-boolean) environment variable exists
- * @param {string | null} variable
- * @returns {boolean}
- */
-export function nonBoolEnvVarExists (variable: string|null): boolean {
-	return variable != null && variable !== "";
-}
