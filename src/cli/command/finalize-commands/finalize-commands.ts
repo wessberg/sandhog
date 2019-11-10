f4f8e0e7947b1658c6689b9a8b75d994a14c4633
wessberg <frederikwessberg@gmail.com>
2019-11-10 01:12:37 +0100

    style(lint): fix lint

--- src/cli/command/finalize-commands/finalize-commands.ts
+++ src/cli/command/finalize-commands/finalize-commands.ts
@@ -2,11 +2,9 @@ import commander from "commander";
 
 commander.parse(process.argv);
 const lastArgv = process.argv.slice(-1)[0];
-if (lastArgv.endsWith("scaffold")) {
+if (lastArgv.endsWith("scaffold") && commander.args.length === 0) {
 	// Show help if no arguments are given
-	if (commander.args.length === 0) {
-		commander.help(text => {
-			return `Welcome to Scaffold!\n\n` + text;
-		});
-	}
+	commander.help(text => {
+		return `Welcome to Scaffold!\n\n` + text;
+	});
 }
